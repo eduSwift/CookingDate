a80e5baa6a39b6283555038cdd3072bb39cc9922
Eduardo Rodrigues <arpegius2008@yahoo.com>
2025-02-11 14:05:44 +0100

    Set up project.

---
+++ CookingDate/CookingDate/CookingDateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingDateApp.swift
+//  CookingDate
+//
+//  Created by Eduardo Rodrigues da Cruz on 11.02.25.
+//
+
+import SwiftUI
+
+@main
+struct CookingDateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
