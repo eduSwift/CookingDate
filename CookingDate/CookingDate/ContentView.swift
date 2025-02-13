7b04a72c03593b3b9f203a5be76b14aaf3888fc2
Eduardo Rodrigues <arpegius2008@yahoo.com>
2025-02-13 15:01:52 +0100

    Install Firebase

--- CookingDate/CookingDate/ContentView.swift
+++ CookingDate/CookingDate/ContentView.swift
@@ -10,20 +10,31 @@ import SwiftUI
 struct ContentView: View {
     let images = ["HT", "GY", "LB"]
     
-        var body: some View {
-            ScrollView(.horizontal){
-                            LazyHStack{
-                                ForEach(images, id: \.self){ image in
-                                    Image(image)
-                                        .containerRelativeFrame(.horizontal)
-                                        .clipShape(RoundedRectangle(cornerRadius: 20))
-                                }
-                            }
+    var body: some View {
+        ScrollView(.horizontal){
+            LazyHStack{
+                ForEach(images, id: \.self){ image in
+                    Image(image)
+                        .containerRelativeFrame(.horizontal)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .scrollTransition { content, phase in
+                            content
+                                .scaleEffect(
+                                    x: phase.isIdentity ? 1.0 : 0.8,
+                                    y: phase.isIdentity
+                                    ? 1.0 : 0.8
+                                )
+                                .offset(y: phase.isIdentity ? 0: 200)
                         }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .background(LinearGradient.appBackground)
+                }
+            }
+           
         }
+        .contentMargins(20)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(LinearGradient.appBackground)
     }
+}
 
 #Preview {
     ContentView()
