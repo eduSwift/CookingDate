7b5e0b67a9ee0447877f543d86aff56deba5d43e
Eduardo Rodrigues <arpegius2008@yahoo.com>
2025-02-17 13:23:23 +0100

    Create SignUpView

---
+++ CookingDate/CookingDate/Views/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  CookingDate
+//
+//  Created by Eduardo Rodrigues da Cruz on 17.02.25.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
